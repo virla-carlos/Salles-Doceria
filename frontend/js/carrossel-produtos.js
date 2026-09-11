4a047bea1836cc37693d9e988b539775c29b489e
c-carl <carlosvirla10@gmail.com>
2026-09-11 14:27:39 -0300

    fix: ajusta largura dos cards do carrossel

--- frontend/js/carrossel-produtos.js
+++ frontend/js/carrossel-produtos.js
@@ -8,7 +8,7 @@ const carrosseis = {
 };
 
 function getVisiveis() {
-    if (window.innerWidth >= 1024) return 3;
+    if (window.innerWidth >= 1024) return 4;
     if (window.innerWidth >= 640) return 2;
     
     return 1;
